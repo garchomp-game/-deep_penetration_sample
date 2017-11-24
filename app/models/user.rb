e6d60e574d6832a76908e2d372667c5122d8a4b8
garchomp-game <otoka.japanesedrum@gmail.com>
2017-11-24 17:19:21 +0900

    メモ追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :microposts, depedent: :destroy
+  has_many :microposts, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, foreign_key: "followed_id",
