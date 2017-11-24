afe15a7073d344e505820e54e08e2fdb3b5ec6c8
garchomp-game <otoka.japanesedrum@gmail.com>
2017-11-24 14:40:17 +0900

    ７章１５節：マイクロポストのモデルを作る。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   end
   def show
     @user=User.find(params[:id])
+    @microposts=@user.microposts.paginate(page: params[:page])
   end
   def destroy
     User.find(params[:id]).destroy
