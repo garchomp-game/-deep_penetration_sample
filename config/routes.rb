5206a2d87c11a87d406ca88f225a4764504d85d3
garchomp-game <otoka.japanesedrum@gmail.com>
2017-11-28 11:03:26 +0900

    7章２２節：フォロー、アンフォローの実装２

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     end
   end
   resources :microposts, onry: [:craete, :destroy]
+  resources :relationships, onry: [:craete, :destroy]
   root 'static_pages#home'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
